e50c1ecf9a30dba45bbb1a73055d6866a1d9eff0
Roberto Pedroso <roberto@rpedroso.com>
2021-04-18 23:25:02 -0400

    Implement consolidated Pokemon 5e Module
    This is the first draft of a module that consolidates some older Pokemon
    5e modules into a more advanced module with more features.

---
+++ images/badges/misc/readme.md
@@ -0,0 +1,11 @@
+# Unnamed Badges
+
+These badges are not identified by name in the show, but the artist rendered them anyways.
+
+I have taken the liberty of giving each of them appropriate names.
+
+Visit here for more information bout where they come from:
+
+https://bulbapedia.bulbagarden.net/wiki/Badge#Gallery_of_unidentified_Badges
+
+- MajorVictory (8/14/2020)
--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,44 @@
-Hooks.once('init', async function() {
+Hooks.once("init", async function () {
+  console.log("Pokemon5e Addons | Initializing");
 
+  CONFIG.DND5E.damageTypes["bug"] = "P5E.DamageBug";
+  CONFIG.DND5E.damageTypes["dark"] = "P5E.DamageDark";
+  CONFIG.DND5E.damageTypes["dragon"] = "P5E.DamageDragon";
+  CONFIG.DND5E.damageTypes["electric"] = "P5E.DamageElectric";
+  CONFIG.DND5E.damageTypes["fairy"] = "P5E.DamageFairy";
+  CONFIG.DND5E.damageTypes["fighting"] = "P5E.DamageFighting";
+  CONFIG.DND5E.damageTypes["flying"] = "P5E.DamageFlying";
+  CONFIG.DND5E.damageTypes["ghost"] = "P5E.DamageGhost";
+  CONFIG.DND5E.damageTypes["grass"] = "P5E.DamageGrass";
+  CONFIG.DND5E.damageTypes["ground"] = "P5E.DamageGround";
+  CONFIG.DND5E.damageTypes["ice"] = "P5E.DamageIce";
+  CONFIG.DND5E.damageTypes["normal"] = "P5E.DamageNormal";
+  CONFIG.DND5E.damageTypes["rock"] = "P5E.DamageRock";
+  CONFIG.DND5E.damageTypes["steel"] = "P5E.DamageSteel";
+  CONFIG.DND5E.damageTypes["water"] = "P5E.DamageWater";
+
+  CONFIG.DND5E.CHARACTER_EXP_LEVELS = [
+    0,
+    200,
+    800,
+    2000,
+    6000,
+    12000,
+    20000,
+    30000,
+    44000,
+    62000,
+    82000,
+    104000,
+    128000,
+    158000,
+    194000,
+    234000,
+    278000,
+    326000,
+    382000,
+    450000,
+  ];
 });
 
-Hooks.once('ready', async function() {
-
-});
+Hooks.once("ready", async function () {});
